0da902bd3e0f7eb1d2a1cfebc8c504472d85a4ef
kkevinlop <114724842+kkevinlop@users.noreply.github.com>
2023-01-14 20:40:39 +0700

    Update Temperature.java

--- src/Temperature/Temperature.java
+++ src/Temperature/Temperature.java
@@ -1,5 +1,8 @@
 package Temperature;
 
+// Temparature as the parent class which consist of variables and method that can be used in the child class.
+// This class combining the usage of inheritance, polymorphism, and abstraction together, which those 3 elements are essential for the child class.
+// This class can also be called as a super class
 public abstract class Temperature {
 	private double temperatureLevel;
 
